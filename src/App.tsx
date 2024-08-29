d52f89fd22b706ac8725fb220992d5bcdf6b3daa
raf0nx <rafalpawlowski2011@gmail.com>
2024-08-29 13:48:34 +0200

    Add a request sending logic to the `Groq` chat completion API

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
+
+import { getGroqChatCompletion } from './ai-helpers'
+
 import './App.css'
 
 function App() {
   const [response, setResponse] = useState('')
 
+  async function sendPrompt() {
+    const chatCompletion = await getGroqChatCompletion()
+    setResponse(chatCompletion.choices[0]?.message?.content || '')
+  }
+
   return (
     <>
       <div className="card">
-        <button onClick={() => setResponse('')}>Send prompt</button>
+        <button onClick={sendPrompt}>Send prompt</button>
       </div>
       <p>Response:</p>
       <p>{response}</p>
---
+++ src/ai-helpers.ts
@@ -0,0 +1,18 @@
+import Groq from 'groq-sdk'
+
+const groq = new Groq({
+  apiKey: import.meta.env.VITE_GROQ_API_KEY,
+  dangerouslyAllowBrowser: true,
+})
+
+export async function getGroqChatCompletion() {
+  return groq.chat.completions.create({
+    messages: [
+      {
+        role: 'user',
+        content: 'Explain the importance of fast language models',
+      },
+    ],
+    model: 'llama3-8b-8192',
+  })
+}
