4105a68fbbcb7965420bf82e6ac5b7a479f858ad
raf0nx <rafalpawlowski2011@gmail.com>
2024-09-04 10:14:56 +0200

    Add canvas background, implement mesh edges

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,32 +1,29 @@
-import { useRef } from 'react'
-import { Canvas as R3Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import { EffectComposer, Outline } from '@react-three/postprocessing'
+import { NearestFilter, TextureLoader } from 'three'
+import { Canvas as R3Canvas, useLoader } from '@react-three/fiber'
+import { Edges, OrbitControls } from '@react-three/drei'
 
 import './Canvas.css'
 
 export function Canvas() {
-  const ref = useRef(null)
+  const notebookTexture = useLoader(TextureLoader, '/squares.png')
+  notebookTexture.minFilter = NearestFilter
+  notebookTexture.magFilter = NearestFilter
 
   return (
     <section className="canvas">
       <R3Canvas>
-        <ambientLight intensity={0.5} />
+        <mesh rotation={[-Math.PI / 2, 0, 0]}>
+          <planeGeometry args={[96, 54]} />
+          <meshStandardMaterial map={notebookTexture} />
+        </mesh>
+        <ambientLight intensity={1} />
         <directionalLight position={[5, 5, 5]} intensity={1} />
-        <mesh ref={ref}>
+        <mesh position={[0, 0.51, 0]}>
           <boxGeometry />
           <meshStandardMaterial color={0xffa500} />
+          <Edges linewidth={2} threshold={1} color={0xffffff} />
         </mesh>
         <OrbitControls />
-        <EffectComposer autoClear={false}>
-          <Outline
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            selection={ref as unknown as any}
-            blur={false}
-            visibleEdgeColor={0xffffff}
-            edgeStrength={0.5}
-          />
-        </EffectComposer>
       </R3Canvas>
     </section>
   )
