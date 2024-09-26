6997e299184c2ab5e0e91049de84a62bc609db20
raf0nx <rafalpawlowski2011@gmail.com>
2024-09-26 20:59:26 +0200

    Extract `Cargo` position adjusting logic to the custom hook

--- src/components/Cargo/Cargo.tsx
+++ src/components/Cargo/Cargo.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useRef, useState } from 'react'
 import { Box, Edges } from '@react-three/drei'
-import * as THREE from 'three'
 
-import { CARGO_EDGE, TRAILER } from '../../helpers/constants'
+import { CARGO_EDGE } from '../../helpers/constants'
 import type { DIMENSIONS_3D } from '../../helpers/types'
+
+import { useAdjustedPosition } from './useAdjustedPosition'
 
 interface CargoProps {
   dimensions: DIMENSIONS_3D
@@ -11,30 +11,8 @@ interface CargoProps {
   color: string
 }
 
-const TRAILER_WIDTH = TRAILER.dimensions[2]
-
 export function Cargo({ dimensions, position, color }: CargoProps) {
-  const boxRef = useRef<THREE.Mesh>(null)
-  const [adjustedPosition, setAdjustedPosition] = useState(position)
-
-  useEffect(() => {
-    if (!boxRef.current) return
-
-    const boundingBox = new THREE.Box3().setFromObject(boxRef.current)
-    const size = new THREE.Vector3()
-
-    boundingBox.getSize(size)
-
-    const halfLength = size.x / 2
-    const halfWidth = size.z / 2
-    const adjustedZPosition = TRAILER_WIDTH / 2 - halfWidth
-
-    setAdjustedPosition(initialPosition => {
-      const [x, y, z] = initialPosition
-
-      return [halfLength + x, y, adjustedZPosition - z]
-    })
-  }, [])
+  const [adjustedPosition, boxRef] = useAdjustedPosition(position)
 
   return (
     <Box ref={boxRef} args={dimensions} position={adjustedPosition}>
