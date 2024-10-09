5fe90f61dd70d50bf9ff9f806e1498370a2e52d4
raf0nx <rafalpawlowski2011@gmail.com>
2024-10-09 10:32:34 +0200

    Create Cargo types and mocked data

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -1,2 +1,18 @@
 export type DIMENSIONS_2D = [number, number]
 export type DIMENSIONS_3D = [number, number, number]
+
+interface CARGO_DATA {
+  name: string
+  color: string
+  dimensions: DIMENSIONS_3D
+}
+
+interface CARGO_DATA_WITH_ID extends CARGO_DATA {
+  id: string
+}
+
+export interface CARGO {
+  [id: string]: CARGO_DATA
+}
+
+export type CARGO_ARRAY = CARGO_DATA_WITH_ID[]
