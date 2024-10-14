14fa25adb94eb697ace47447fe2f798b27a4fa69
raf0nx <rafalpawlowski2011@gmail.com>
2024-10-14 11:24:03 +0200

    Add empty line

--- src/components/Tools/helpers.ts
+++ src/components/Tools/helpers.ts
@@ -15,5 +15,6 @@ export function generateCargoId() {
 
 export function generateRandomHexColor() {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16)
+
   return `#${randomColor.padStart(6, '0')}`
 }
