65d6165a149455afba379a464ba68bbce6586da6
raf0nx <rafalpawlowski2011@gmail.com>
2024-10-22 17:36:22 +0200

    Render cargo distribution in the `Canvas`

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -5,10 +5,13 @@ import { Base } from '../Base'
 import { Cargo } from '../Cargo'
 import { CargoContainer } from '../CargoContainer'
 import { CAMERA, LIGHT } from '../../helpers/constants'
+import { useCargoDistributionContext } from '../../hooks'
 
 import './Canvas.css'
 
 export function Canvas() {
+  const { cargoDistribution } = useCargoDistributionContext()
+
   return (
     <section className="canvas">
       <R3Canvas camera={CAMERA.initialPosition}>
@@ -19,7 +22,15 @@ export function Canvas() {
         />
         <Base />
         <CargoContainer />
-        <Cargo dimensions={[1, 1, 1]} position={[0, 0, 0]} color="orange" />
+        {/* TODO: when you edit existing cargo its useAdjustedPosition doesnt recalculate leading to weird errors */}
+        {Object.entries(cargoDistribution).map(([cargoID, cargo]) => (
+          <Cargo
+            key={cargoID}
+            dimensions={cargo.dimensions}
+            position={cargo.position}
+            color={cargo.color}
+          />
+        ))}
         <OrbitControls
           minDistance={CAMERA.minDistance}
           maxDistance={CAMERA.maxDistance}
